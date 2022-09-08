3d0c23163714a9c74b92eaed3350740f02c51a1f
Grant.B <Grant.b2134@gmail.com>
2022-09-08 12:54:52 -0400

    Comment query fix

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -12,23 +12,29 @@ export default async function comments(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const graphQLClient = new GraphQLClient(graphqlAPI, { 
-    headers: {
-      authorization: `Bearer ${graphcmsToken}`
-    }
-  })
+  if (graphqlAPI){
+      const graphQLClient = new GraphQLClient(graphqlAPI, { 
+        headers: {
+          authorization: `Bearer ${graphcmsToken}`
+        }
+      })
+    
 
-
-  const query = gql `
-    mutation CreateComment ( $name: String!, $email: String!, $comment: String!, $slug: String! ) {
-      createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug }}}) {id}
+    const query = gql `
+      mutation CreateComment ( $name: String!, $email: String!, $comment: String!, $slug: String! ) {
+        createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug }}}) {id}
+      }
+    `
+    try {
+      const result = await graphQLClient.request(query, req.body)
+      return res.status(200).send(result);
+    } catch (error) {
+      if (error){
+        return res.status(500).send({
+          name: "unable to create comment",
+          ...error
+        }) 
+      }
     }
-  `
-  try {
-    const result = await graphQLClient.request(query, req.body)
-    return res.status(200).send(result);
-  } catch (error) {
-    console.log(error)
-    return res.status(500).send(error)
   }
 }
