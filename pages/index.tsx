bb94d8c94fbec715e071d3ec1a25675b0dc15384
Grant.B <Grant.b2134@gmail.com>
2022-09-08 15:01:50 -0400

    Comments fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,13 @@ import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
 import { getPosts } from '../services'
 import { FeaturedPosts } from '../sections'
+import { Key } from 'react'
 
+interface Props {
+  posts:Array<object>;
+}
 
-const Home: NextPage = ( {posts} ) => {
+const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -15,13 +19,13 @@ const Home: NextPage = ( {posts} ) => {
       <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => 
+          {posts.map((post:any) => 
             <PostCard post={post.node} key={post.title} />
           )}
         </div>
         <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relatve top-8'>
-              <PostWidget />
+              <PostWidget categories={undefined} slug={undefined} />
               <Categories />
             </div>
         </div>
