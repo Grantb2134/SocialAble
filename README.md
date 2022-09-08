ea04e5e25f803f4f420f6ccd2238da296614dab1
Grant.B <Grant.b2134@gmail.com>
2022-09-08 01:50:01 -0400

    pushing project from my local

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SocialAble
+Blog
---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout } from '../components';
+import '../styles/globals.scss'
+import { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+
+  )
+}
+
+export default MyApp
